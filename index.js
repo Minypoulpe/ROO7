8f291b63c840b36e4d68f01398f052fc4b650fa0
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-09-29 22:18:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require("./discord.js");
 const botconfig = require("botconfig.json");
 const bot = new Discord.Client({disableEveryone: true});
 
