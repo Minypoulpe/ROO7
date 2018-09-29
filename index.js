b37e93df21e0cc1dec2af84faca6f96e22fc6354
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-09-29 23:01:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,38 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   
-   if (cmd === `${prefix}end`){
+  const Discord = require("discord.js");
+const fs = require("fs");
+
+module.exports.run = async (bot, message, args) => {
+
+
+  if(!args[0] || args[0 == "help"]) return message.reply("Usage: !prefix <desired prefix here>");
+
+  let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+  prefixes[message.guild.id] = {
+    prefixes: args[0]
+  };
+
+  fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
+    if (err) console.log(err)
+  });
+
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("#FF9900")
+  .setTitle("Prefix Set!")
+  .setDescription(`Set to ${args[0]}`);
+
+  message.channel.send(sEmbed);
+
+}
+
+module.exports.help = {
+  name: "prefix"
+}
+
+   else if (cmd === `${prefix}end`){
    let role = message.guild.roles.find('name', 'Terrarien')
    let role2 = message.guild.roles.find('name', 'Terramerde')
    let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
