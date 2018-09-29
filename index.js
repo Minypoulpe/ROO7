16bdf6a6331c9125096035f733a1938a01d0b151
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-09-29 22:58:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const fs = require("fs");
+bot.commands = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
 
