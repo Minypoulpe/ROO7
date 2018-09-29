b89e484e681c5b1ec3751b303642d4b706ddba5b
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-09-29 13:45:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} is online!`);
-  bot.user.setActivity(`Hey, Wassup!`);
+  bot.user.setActivity(`ROOOOOOO7`);
 });
 
 bot.on("message", async message => {
@@ -11,13 +11,13 @@ bot.on("message", async message => {
   if (message.author.bot) return;
   if (message.channel.type === "dm") return;
 
-  let prefix = '-';
+  let prefix = '!';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
 
-  if (cmd === `${prefix}ping`){
-    message.channel.send("Pong!");
+  if (cmd === `${prefix}say`){
+    message.channel.send("nope");
   }
 });
 
