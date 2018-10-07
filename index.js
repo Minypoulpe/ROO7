cc27220f13c9fd996cea557f1e371d382a4b8323
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-10-07 09:46:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,8 @@ let messageArray = message.content.split(" ");
 let command = messageArray[0];
 let args = messageArray.slice(1);
   
- if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Tu n'as pas la permission de faire sa /:.");
   
- else if(command === `${prefix}terramerde`) {
+  if(command === `${prefix}terramerde`) {
 
   let role = message.guild.roles.find('name', 'Terrarien')
   let role2 = message.guild.roles.find('name', 'Terramerde')
