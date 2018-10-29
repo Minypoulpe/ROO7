2b5a17d82c20b52e411237fcee7593357245bd7e
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-10-29 18:45:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ let command = messageArray[0];
 let args = messageArray.slice(1);
   
   if(command === `${prefix}terramerde`) {
-  let role = message.guild.roles.find('name', 'Terrarien')
-  let role2 = message.guild.roles.find('name', 'Terramerde')
+     let role = message.guild.roles.find('name', 'Terrarien')
+     let role2 = message.guild.roles.find('name', 'Terramerde')
 
 
 
-  let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
-  if(!pUser) return message.channel.send("Je ne trouve pas ce membre");
+     let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+     if(!pUser) return message.channel.send("Je ne trouve pas ce membre");
 
-  if(message.member.roles.find('name', 'Terramerde')){
-    message.channel.send("Il a déjà ce rôle")
-  }
+    if(message.member.roles.find('name', 'Terramerde')){
+       message.channel.send("Il a déjà ce rôle")
+    }
 
-  else{
-    pUser.addRole(role2);
-    pUser.removeRole(role);
-    message.channel.send(`${pUser} a reçu le rôle Terramerde. Merci de lui enlever dans 1 semaine et de lui remettre le rôle Terrarien.`);
-    pUser.send(`Tu as reçu le rôle Terramerde car tu n'as pas respecter une des règles. Tu devrais les relire dans le salon #règles. Ton rôle sera retirer dans une semaine`)
-  }
+    else{
+        pUser.addRole(role2);
+        pUser.removeRole(role);
+        message.channel.send(`${pUser} a reçu le rôle Terramerde. Merci de lui enlever dans 1 semaine et de lui remettre le rôle Terrarien.`);
+        pUser.send(`Tu as reçu le rôle Terramerde car tu n'as pas respecter une des règles. Tu devrais les relire dans le salon #règles. Ton rôle sera retirer dans une semaine`)
+   }
   
 });
 
