38f3b9b3391f9b88242ca4280fbc6c19f076ae39
Minypoulpe <43681691+Minypoulpe@users.noreply.github.com>
2018-11-17 23:30:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ let prefix = "!";
 let messageArray = message.content.split(" ");
 let command = messageArray[0];
 let args = messageArray.slice(1);
+let pUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+if(!pUser) return message.channel.send("Je ne trouve pas ce membre");
 let role = message.guild.roles.find('name', 'Terrarien')
 let role2 = message.guild.roles.find('name', 'Terramerde')
   
-  if(command === `everyone`) {
-    message.author.addRole(role2)
-    message.author.removeRole(role)
+  if(message.member.roles.find('name', 'Terramerde')){
+    message.channel.send("Il a déjà ce rôle")
   }
-  
+
+  else{
+    pUser.addRole(role2);
+    pUser.removeRole(role);
+    message.channel.send(`${pUser} a reçu le rôle Terramerde. Merci de lui enlever dans 1 semaine et de lui remettre le rôle Terrarien.`);
+    pUser.send(`Tu as reçu le rôle Terramerde car tu n'as pas respecter une des règles. Tu devrais les relire dans le salon #règles. Ton rôle sera retirer dans une semaine`)
+  }
 });
 
 bot.login(process.env.token);
